291d35f69c70bc7c430ef31f4eb520f328c46cd7
hufei <hufeispace@gmail.com>
2019-05-20 05:56:30 +0800

    format file

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const babel = require('@babel/core');
-const { parse } = require("@babel/parser") ;
 const t = require('@babel/types');
-const { default: traverse } = require("@babel/traverse") ;
-const fs = require('fs');
-const path = require('path');
+const { default: traverse } = require("@babel/traverse");
 
 const extend = require('extend');
 
@@ -18,7 +15,7 @@ const config = {
 				useBuiltIns: false,
 				targets: {
 					// The % refers to the global coverage of users from browserslist
-					browsers: [ 'defaults', 'chrome >= 49', 'ie > 8', 'edge > 11', 'safari > 9', 'not op_mini all' ]
+					browsers: ['defaults', 'chrome >= 49', 'ie > 8', 'edge > 11', 'safari > 9', 'not op_mini all']
 				}
 			}
 		]
@@ -38,20 +35,12 @@ const config = {
 	ast: true
 };
 
-const requireAsyncWrap = require('../babel-plugin-stop-hosit/index.js');
-
-module.exports = function(content, file, options) {
+module.exports = function (content, file, options) {
 	const conf = mergeConf(options, file);
 	const result = babel.transformSync(content, conf);
-	if (file.isPartial) {
-		// const ast = parser.parse(result.code);
-		// traverse.default(ast, requireAsyncWrap.visitor);
-		// return babel.transformFromAstSync(ast).code;
-		require('fs').writeFileSync(`result-ast-${file.basename}.json`, JSON.stringify(result.ast));
-		// let ast = result.ast;
-		requireAsync(result.ast);
+	if (file.isPartial) { // 处理html,vm中的script脚本
+		requireAsync(result.ast); // 处理<script>中的require
 		return babel.transformFromAstSync(result.ast).code
-		// return result.code;
 	} else {
 		return result.code;
 	}
@@ -60,7 +49,6 @@ module.exports = function(content, file, options) {
 function mergeConf({ presets = [], plugins = [], sourceMap }, file) {
 	const conf = extend(
 		{
-			// filename: file.subpath,
 			sourceFileName: file.basename
 		},
 		config,
@@ -70,23 +58,15 @@ function mergeConf({ presets = [], plugins = [], sourceMap }, file) {
 			plugins: config.plugins.concat(plugins)
 		}
 	);
-
-	// if(file.isPartial){
-	// 	const extra = require('../babel-plugin-stop-hosit/index.js');
-	// 	conf.presets.unshift({
-	// 		plugins: [ extra ]
-	// 	});
-	// }
 	return conf;
 }
 
 
-function requireAsync(ast){
+function requireAsync(ast) {
 	const collectRequire = {
 		CallExpression: {
 			exit(path) {
 				if (path.get('callee').node.name === 'require') {
-					console.log(path.get('arguments')[0].node.value);
 					requires.unshift(path);
 				}
 			}
@@ -94,101 +74,82 @@ function requireAsync(ast){
 	};
 
 	traverse(ast, {
-		// visitor: {
-			Program: {
-				enter(path, state) {
-					ra = null;
-					requires = [];
-				},
-				exit(path, state) {
-					// console.log(JSON.stringify(state.file.ast));
-					path.traverse(collectRequire);
-	
-					if (requires.length > 0) {
-						let hasRA = path.get('body').some((n) => {
-							if (
-								n.type === 'ExpressionStatement' &&
-								n.node.expression.type === 'CallExpression' &&
-								n.node.expression.callee &&
-								n.node.expression.callee.type === 'MemberExpression' &&
-								n.node.expression.callee.object.name === 'require' &&
-								n.node.expression.callee.property.name === 'async'
-							) {
-								ra = n;
-								return true;
-							}
-						});
-	
-						if (hasRA) {
-							console.log('\n\n\n\n\nhasRa +++++++ ----\n\n\n\n\n');
-							let n = ra;
-							let raArgs = [],
-								raCallback = null;
-							if (ra.node.expression.arguments[0].type === 'ArrayExpression') {
-								raArgs = ra.node.expression.arguments[0];
-								raCallback = ra.node.expression.arguments[1];
-							}
-	
-							requires.reverse().forEach((n, i) => {
-								let args = n.get('arguments');
-								args && raArgs.elements.unshift(args[0].node);
-	
-								const varObj = path.scope.generateUidIdentifier('_require');
-	
-								raCallback.params.unshift(varObj);
-	
-								n.replaceWith(varObj);
-							});
-	
-							const allPrevs = ra.getAllPrevSiblings();
-							allPrevs.reverse().forEach((n) => {
-								raCallback.body.body.unshift(n.node);
-								n.remove();
-							});
-						} else {
-							// writeFile(generateFile('before')(), JSON.stringify(state.file.ast))
-							// .then((d) => {
-							// 	console.log(`write file's ast : ${state.filename} success\n`);
-							// })
-							// .finally(() => {
-							// 	console.log(`[${new Date()}] write before ast error~`);
-							// });
-							const rArgs = [],
-								params = [];
-							requires.forEach((n) => {
-								let args = n.get('arguments');
-								args && rArgs.push(args[0].node);
-	
-								const varObj = path.scope.generateUidIdentifier('_require');
-	
-								params.push(varObj);
-	
-								// TODO: if n is like to 'require('xxxx')', then should delete it
-								n.replaceWith(varObj); // var xxx = require('xxxx');
-							});
-	
-							// // create a new require.async(['x',...], function(x,...){});
-							const newRa = t.expressionStatement(
-								t.callExpression(t.memberExpression(t.identifier('require'), t.identifier('async')), [
-									t.arrayExpression(rArgs),
-									t.functionExpression(null, params, t.blockStatement(path.node.body))
-								])
-							);
-	
-							// path.unshiftContainer('body', newRa);
-	
-							path.node.body = [ newRa ];
+		Program: {
+			enter(path, state) {
+				ra = null;
+				requires = [];
+			},
+			exit(path, state) {
+				path.traverse(collectRequire);
+
+				if (requires.length > 0) { 
+
+					// 检查是否已经存在require.async
+					let hasRA = path.get('body').some((n) => {
+						if (
+							n.type === 'ExpressionStatement' &&
+							n.node.expression.type === 'CallExpression' &&
+							n.node.expression.callee &&
+							n.node.expression.callee.type === 'MemberExpression' &&
+							n.node.expression.callee.object.name === 'require' &&
+							n.node.expression.callee.property.name === 'async'
+						) {
+							ra = n;
+							return true;
 						}
-	
-						// writeFile(generateFile('after')(), JSON.stringify(ast))
-						// 	.then((d) => {
-						// 		console.log(`write file's ast : xxxxx.json success\n`);
-						// 	})
-						// 	.finally(() => {
-						// 		console.log(`[${new Date()}] write after ast error~`);
-						// 	});
+					});
+
+					// 如果存在require.async， 则把收集到的require('xxx')，直接放到require.async中
+					if (hasRA) {
+						let raArgs = [],
+							raCallback = null;
+						if (ra.node.expression.arguments[0].type === 'ArrayExpression') {
+							raArgs = ra.node.expression.arguments[0];
+							raCallback = ra.node.expression.arguments[1];
+						}
+
+						requires.reverse().forEach((n, i) => {
+							let args = n.get('arguments');
+							args && raArgs.elements.unshift(args[0].node);
+
+							const varObj = path.scope.generateUidIdentifier('_require');
+
+							raCallback.params.unshift(varObj);
+
+							n.replaceWith(varObj);
+						});
+
+						const allPrevs = ra.getAllPrevSiblings();
+						allPrevs.reverse().forEach((n) => {
+							raCallback.body.body.unshift(n.node);
+							n.remove();
+						});
+					} else {
+						const rArgs = [],
+							params = [];
+						requires.forEach((n) => {
+							let args = n.get('arguments');
+							args && rArgs.push(args[0].node);
+
+							const varObj = path.scope.generateUidIdentifier('_require');
+
+							params.push(varObj);
+
+							// TODO: if n is like to 'require('xxxx')', then should delete it
+							n.replaceWith(varObj); // var xxx = require('xxxx');
+						});
+
+						// // create a new require.async(['x',...], function(x,...){});
+						const newRa = t.expressionStatement(
+							t.callExpression(t.memberExpression(t.identifier('require'), t.identifier('async')), [
+								t.arrayExpression(rArgs),
+								t.functionExpression(null, params, t.blockStatement(path.node.body))
+							])
+						);
+						path.node.body = [newRa];
 					}
 				}
 			}
+		}
 	});
 }
--- test/s.js
+++ test/s.js
@@ -1,5 +1,10 @@
-var _interopRequireDefault = require("test:node_modules/@babel/runtime-corejs3/helpers/interopRequireDefault");
-
-var _promise = _interopRequireDefault(require("test:node_modules/@babel/runtime-corejs3/core-js-stable/promise"));
-
-_promise.default.resolve(1); 
+require.async(["node_modules/@babel/runtime-corejs3/core-js-stable/promise",
+"booobkbklksasakdt",
+"node_modules/@babel/runtime-corejs3/helpers/interopRequireDefault"
+], function (_require, _require2) {
+    var _interopRequireDefault = _require2;
+  
+    var _promise = _interopRequireDefault(_require);
+  
+    _promise.default.resolve('a');
+  });
